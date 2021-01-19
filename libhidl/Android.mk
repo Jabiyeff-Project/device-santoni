pratiksharma269/device_xiaomi_santoni

santoni: libhidl: Add hidl manager target
Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

libhidl/Android.mk
@@ -14,7 +14,16 @@
 # limitations under the License.
 
 include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.base@1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidlbase
+LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true

Jabiyeff-Project/device-santoni

Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

libhidl/Android.mk
@@ -20,3 +20,11 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.manager@1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)