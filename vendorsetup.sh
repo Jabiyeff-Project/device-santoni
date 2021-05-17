a9909b21f0599f4bd81c893725fa8fdab89aadf8
Jabiyeff <cebiyevanar@gmail.com>
2021-05-18 01:09:23 +0400

    santoni: Use my kernel source if custom kernel doesn't available
    * Also, update vendorsetup layout
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,23 @@
 if [ ! -f .repo/local_manifests/santoni.xml ]; then
-echo "Clone local_manifests"
-git clone -b 11.0 https://github.com/Jabiyeff-Project/local_manifests.git .repo/local_manifests/
+    echo "Clone local_manifests"
+    git clone -b 11.0 https://github.com/Jabiyeff-Project/local_manifests.git .repo/local_manifests/
 else
-echo "Pull local_manifests"
-cd .repo/local_manifests
-git pull
-cd ../..
+    echo "Pull local_manifests"
+    cd .repo/local_manifests
+    git pull
+    cd ../..
+fi
+
+if [ ! -f kernel/xiaomi/msm8937/Makefile ]; then
+    echo "Using default kernel"
+    export TARGET_KERNEL_SOURCE=kernel/xiaomi/santoni
+    export TARGET_KERNEL_CONFIG=santoni_treble_defconfig
+else
+    echo "Using custom kernel"
+    export TARGET_KERNEL_SOURCE=kernel/xiaomi/msm8937
+    if [ -f kernel/xiaomi/msm8937/arch/arm64/configs/santoni_treble_defconfig ]; then
+        export TARGET_KERNEL_CONFIG=santoni_treble_defconfig
+    else
+        export TARGET_KERNEL_CONFIG=santoni_defconfig
+    fi
 fi
