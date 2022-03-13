edb5286d487e62328e3f9e7d2b3ff8c5dcfcd5d4
Dinesh <101502111+Dinesh-MSDian@users.noreply.github.com>
2022-03-13 15:46:00 +0530

    Delete bliss_santoni.mk

--- bliss_santoni.mk
+++
@@ -1,6 +0,0 @@
-# Inherit makefiles
-$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
-
-# ROM spesific makefile name
-PRODUCT_NAME := bliss_santoni
