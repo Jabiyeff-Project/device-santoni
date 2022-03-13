9cc6d42d648a92ebfbeeda57788428c9fcb4b09a
Dinesh <101502111+Dinesh-MSDian@users.noreply.github.com>
2022-03-13 15:45:40 +0530

    Delete arrow_santoni.mk

--- arrow_santoni.mk
+++
@@ -1,6 +0,0 @@
-# Inherit makefiles
-$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-# ROM spesific makefile name
-PRODUCT_NAME := arrow_santoni
